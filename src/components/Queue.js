f3e0389cce809ef28475d63ca4452a2bbff8c67c
Sahil Samantaray <sahil13399@gmail.com>
2021-05-11 12:24:02 +0530

    Added form features and file upload

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
@@ -11,13 +11,25 @@ import Table2 from './Table2'
 
 
 function Queue() {
-
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [cName, setCName] = useState("");
+    const [cEmail, setCEmail] = useState("");
+    const [cPhone, setCPhone] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
     const [turnOver, setState] = React.useState("");
     const handleChange = (event) => {      
         const turnOver = event.target.value;
         setState(turnOver);
     };
 
+    const handleFileInput = (e) => {
+        // handle validations
+        const file = e.target.files[0];
+        //console.log(file)
+       setSelectedFile(file)
+      };
 
     return (
         <div className = "Queue" style = {{marginLeft:80,marginRight:50,display:'flex',flexDirection:'column'}}>
@@ -37,16 +49,16 @@ function Queue() {
                             <h1 style = {{}}>  Client Information</h1>
                             <div style = {{display : 'flex',justifyContent : 'space-between',marginTop:50}}>
 
-                                <TextField style = {{marginRight:80}} placeholder = "Enter Name"/>
-                                <TextField style = {{marginRight:80}} placeholder = "Enter Email"/>
-                                <TextField style = {{marginRight:80}} placeholder = "Enter Phone No"/>
-                                <Button variant="contained" color="red"><AddIcon /> Attachments</Button>
+                                <TextField style = {{marginRight:80}} placeholder = "Enter Name" value={name} onChange={(e) => setName(e.target.value)}/>
+                                <TextField style = {{marginRight:80}} placeholder = "Enter Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <TextField style = {{marginRight:80}} placeholder = "Enter Phone No" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                                <Button variant="contained" color="red" component="label"><AddIcon /> Attachments <input type="file" hidden /></Button>
                             </div>
                             <div style = {{display : 'flex',marginTop:50,marginRight:50}}>
 
-                                <TextField  style = {{marginRight:80}} placeholder = "Enter Name"/>
-                                <TextField  style = {{marginRight:80}} placeholder = "Enter Name"/>
-                                <TextField  style = {{marginRight:80}} placeholder = "Enter Name"/>
+                                <TextField  style = {{marginRight:80}} placeholder = "Enter Company Name" value={cName} onChange={(e) => setCName(e.target.value)}/>
+                                <TextField  style = {{marginRight:80}} placeholder = "Enter Company Email" value={cEmail} onChange={(e) => setCEmail(e.target.value)}/>
+                                <TextField  style = {{marginRight:80}} placeholder = "Enter Company Phone No" value={cPhone} onChange={(e) => setCPhone(e.target.value)}/>
                             </div>
                             <div style = {{marginTop:50,marginRight:50,width:200}}>
                             <InputLabel id="demo-simple-select-filled-label">Turn Over Level</InputLabel>
